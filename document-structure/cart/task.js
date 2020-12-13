0a3e7a97f8069a6666fdb57b3ac85b35e43f515c
deizee <deizee@mail.ru>
2020-12-13 22:15:47 +0500

    Задача 3. "Корзина товаров"

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,7 +1,25 @@
 const productsContainer = document.querySelector('.products');
 const cartContainer = document.querySelector('.cart__products');
+const cartTitle = document.querySelector('.cart__title');
 
+document.addEventListener('DOMContentLoaded', onLoad);
 productsContainer.addEventListener('click', onProductsContainerClick);
+cartContainer.addEventListener('click', onCartContainerClick);
+
+function onLoad() {
+    let productsList = localStorage.getItem('myProductsList');
+    productsList = productsList ? JSON.parse(productsList) : {};
+    if (Object.keys(productsList).length == 0) {
+        cartTitle.classList.add('invisible');
+    } else {
+        cartTitle.classList.remove('invisible');
+    }
+    Object.values(productsList).forEach(el => {
+        const {id, img, quantity} = el;
+        const template = getProductTemplate(id, img, quantity);
+        cartContainer.insertAdjacentHTML('afterbegin', template);
+    })
+}
 
 function onProductsContainerClick(e) {
     if (e.target.classList.contains('product__quantity-control')) {
@@ -9,6 +27,12 @@ function onProductsContainerClick(e) {
     }
     if (e.target.classList.contains('product__add')) {
         onAddBtnClick(e.target);
+    }
+}
+
+function onCartContainerClick(e) {
+    if (e.target.classList.contains('product__del')) {
+        onDelBtnClick(e.target);
     }
 }
 
@@ -40,7 +64,21 @@ function onAddBtnClick(target) {
         cartContainer.insertAdjacentHTML('beforeend', template);
     };
 
+    cartTitle.classList.remove('invisible');
+
+    showAnimation(parent, productId);
+
     addProductsToLocalStorage(productId, productImg, quantity);
+}
+
+function onDelBtnClick(target) {
+    const parent = target.closest('.cart__product');
+    const productId = parent.dataset.id;
+    delProductFromLocalStorage(productId);
+    target.closest('.cart__product').remove();
+    if (!cartContainer.querySelector('.cart__product')) {
+        cartTitle.classList.add('invisible');
+    }
 }
 
 function getProductTemplate(productId, productImg, quantity) {
@@ -48,21 +86,73 @@ function getProductTemplate(productId, productImg, quantity) {
         <div class="cart__product" data-id="${productId}">
             <img class="cart__product-image" src="${productImg}">
             <div class="cart__product-count">${quantity}</div>
+            <div class="product__del">
+                Удалить
+            </div>
         </div>
     `
 }
 
 function addProductsToLocalStorage(productId, productImg, quantity) {
-    let obj = localStorage.getItem('myProductsList');
-    obj = obj ? JSON.parse(obj) : {};
-    if (obj[productId]) {
-        obj[productId].quantity = quantity;
+    let productsList = localStorage.getItem('myProductsList');
+    productsList = productsList ? JSON.parse(productsList) : {};
+    if (productsList[productId]) {
+        productsList[productId].quantity += quantity;
     } else {
-        obj[productId] = {
+        productsList[productId] = {
             id: productId,
             img: productImg,
             quantity,
         };
     }
-    localStorage.setItem('myProductsList', JSON.stringify(obj));
+    localStorage.setItem('myProductsList', JSON.stringify(productsList));
+}
+
+function delProductFromLocalStorage(productId) {
+    let productsList = localStorage.getItem('myProductsList');
+    productsList = productsList ? JSON.parse(productsList) : {};
+    delete productsList[productId];
+    localStorage.setItem('myProductsList', JSON.stringify(productsList));
+}
+
+function showAnimation(parent, productId) {
+    const img = parent.querySelector('img');
+    const imgRect1 = img.getBoundingClientRect();
+    const left1 = imgRect1.left;
+    const top1 = imgRect1.top;
+
+    const imgInCartContainer = cartContainer.querySelector(`.cart__product[data-id="${productId}"]`).querySelector('img');
+    const imgRect2 = imgInCartContainer.getBoundingClientRect();
+    const left2 = imgRect2.left;
+    const top2 = imgRect2.top;
+    
+    const imgCopy = img.cloneNode(true);
+    imgCopy.style.position = 'absolute';
+    imgCopy.style.zIndex = '99';
+    imgCopy.style.left = `${left1}px`;
+    imgCopy.style.top = `${top1}px`;
+
+    document.body.insertAdjacentElement('afterbegin', imgCopy);
+    
+    startAnimation(imgCopy, left1, top1, left2, top2);
+}
+
+function startAnimation(imgCopy, left1, top1, left2, top2) {
+    let x = left2 - left1;
+    let y = top1 - top2;
+    const stepX = x / 50;
+    const stepY = y / 50;
+    let interval = setInterval(() => {
+        if (x > stepX && y > stepY) {
+            left1 += stepX;
+            top1 -= stepY;
+            imgCopy.style.left = `${left1}px`;
+            imgCopy.style.top = `${top1}px`;
+            x -= stepX;
+            y -= stepY;
+        } else {
+            clearInterval(interval);
+            imgCopy.remove();
+        }
+    }, 10);
 }
