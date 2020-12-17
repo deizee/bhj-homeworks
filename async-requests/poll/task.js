f0b4025e0def7402136989c6bccef897a5250a0c
deizee <deizee@mail.ru>
2020-12-17 20:44:14 +0500

    Задача 1. "Опрос с выбором результатов" (*)

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -2,44 +2,64 @@ const poll = document.querySelector('poll');
 const pollTitle = document.getElementById('poll__title');
 const pollAnswers = document.getElementById('poll__answers');
 
+document.addEventListener('DOMContentLoaded', getPoll);
 pollAnswers.addEventListener('click', onPollAnswersClick);
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET','https://netology-slow-rest.herokuapp.com/poll.php');
-xhr.addEventListener('readystatechange', function() {
-    if (this.readyState == xhr.DONE && this.status >= 200 && this.status < 300) {
-        const response = JSON.parse(this.responseText);
-        const { id, data: { title, answers } } = response;
-
-        const titleTemplate = getTitleTemplate(title);
-        pollTitle.insertAdjacentHTML('afterbegin', titleTemplate);
-
-        answers.forEach(answer => {
-            const answerTemplate = getAnswerTemplate(answer);
-            pollAnswers.insertAdjacentHTML('beforeend', answerTemplate);
-        })
-    }});
-xhr.send();
-
-function getTitleTemplate(title) {
-    return `
-        <div class="poll__title" id="poll__title">
-           ${title}
-        </div>
-    `
+function getPoll() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
+  xhr.addEventListener('readystatechange', function () {
+    if (xhr.readyState == xhr.DONE && Math.floor(xhr.status / 100) == 2) {
+      const response = JSON.parse(xhr.responseText);
+      renderPoll(response);
+    }
+  });
+  xhr.send();
 }
 
-function getAnswerTemplate(answer) {
-    return `
-        <button class="poll__answer">
+function renderPoll( { id, data: { title, answers } } ) {
+  pollTitle.textContent = title;
+  pollTitle.dataset.id = id;
+  answers.forEach((answer, id) => {
+    const answerTemplate = getAnswerTemplate(answer, id);
+    pollAnswers.insertAdjacentHTML('beforeend', answerTemplate);
+  })
+}
+
+function getAnswerTemplate(answer, id) {
+  return `
+        <button class="poll__answer" data-id="${id}">
             ${answer}
         </button>
     `
 }
 
 function onPollAnswersClick(e) {
-    if (e.target.classList.contains('poll__answer')) {
-        alert('Спасибоб ваш голос засчитан!');
-    }
+  if (e.target.classList.contains('poll__answer')) {
+    alert('Спасибоб ваш голос засчитан!');
+
+    const pollId = pollTitle.dataset.id;
+    const answerId = e.target.dataset.id;
+    
+    const xhr = new XMLHttpRequest;
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php');
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.addEventListener('readystatechange', function () {
+      if (xhr.readyState == xhr.DONE && Math.floor(xhr.status / 100) == 2) {
+        const response = JSON.parse(xhr.responseText);
+        const template = renderStatistic(response);
+        pollAnswers.innerHTML = template;
+      }
+    });
+    xhr.send(`vote=${pollId}&answer=${answerId}`);
+  }
 }
 
+function renderStatistic( { stat } ) {
+  const total = stat.reduce(((acc, cur) => acc += cur.votes), 0);
+  return stat.map(el => {
+    return `
+      <div>${el.answer}: <span style="font-weight: bold">${((el.votes * 100) / total).toFixed(2)}%</div>
+    `
+  }).join(' ');
+}
