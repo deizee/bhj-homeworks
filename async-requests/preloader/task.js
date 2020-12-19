00375c91e4d2d22acfbcbe7f2ea6ad57885f2d4e
deizee <deizee@mail.ru>
2020-12-19 16:40:20 +0500

    Задача 1. "Анимация загрузки данных" (*)

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,33 +1,63 @@
 const loader = document.getElementById('loader');
 const items = document.getElementById('items');
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET','https://netology-slow-rest.herokuapp.com');
-xhr.addEventListener('readystatechange', function() {
-    if (this.readyState == xhr.DONE && this.status >= 200 && this.status < 300) {
-        const data = JSON.parse(this.responseText);
-        const valute = data.response.Valute;
-        Object.values(valute).forEach(el => {
+document.addEventListener('DOMContentLoaded', getLoader);
+
+function getLoader() {
+    let valuteList = localStorage.getItem('myValuteList');
+    valuteList = valuteList ? JSON.parse(valuteList) : [];
+
+    if (valuteList.length == 0) {
+        getRequest();
+    } else {
+        valuteList.forEach(el => {
             const template = getTemplate(el);
             items.insertAdjacentHTML('beforeend', template);
         });
 
         loader.classList.remove('loader_active');
-    }});
-xhr.send();
+    };
+}
 
-function getTemplate({ CharCode, Value }) {
+function getRequest() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
+    xhr.addEventListener('readystatechange', function () {
+        if (xhr.readyState == xhr.DONE && Math.floor(xhr.status / 100) == 2) {
+            const data = JSON.parse(xhr.responseText);
+            const valute = data.response.Valute;
+
+            Object.values(valute).forEach(el => {
+                const template = getTemplate(el);
+                items.insertAdjacentHTML('beforeend', template);
+                addValuteInLocalStorage(el);
+            });
+
+            loader.classList.remove('loader_active');
+        };
+    });
+    xhr.send();
+}
+
+function getTemplate(el) {
     return `
         <div class="item">
             <div class="item__code">
-                ${CharCode}
+                ${el.CharCode}
             </div>
             <div class="item__value">
-                ${Value}
+                ${el.Value}
             </div>
             <div class="item__currency">
                 руб.
             </div>
         </div>
     `
+}
+
+function addValuteInLocalStorage(el) {
+    let valuteList = localStorage.getItem('myValuteList');
+    valuteList = valuteList ? JSON.parse(valuteList) : [];
+    valuteList.push(el);
+    localStorage.setItem('myValuteList', JSON.stringify(valuteList));
 }
